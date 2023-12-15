11f529c1dacabc515ca3570d6c3fd19677c96c90
arijitk930 <arijitk930@gmail.com>
2023-12-16 01:55:26 +0530

    first commit

---
+++ src/components/TodoItem2.jsx
@@ -0,0 +1,20 @@
+function TodoItem2() {
+  let todoName = "Goto College";
+  let todoDate = "04/11/23";
+
+  return (
+    <div class="container ">
+      <div class="row">
+        <div class="col-6">{todoName}</div>
+        <div class="col-4">{todoDate}</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem2;
